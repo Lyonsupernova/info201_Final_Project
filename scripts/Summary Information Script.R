b55198eb53e8138092873ffee5b2d307197b5680
Lyonsupernova <lyons2000124@outlook.com>
2019-05-21 15:55:37 -0700

    first push plot and summary, please not forget to clone these files

---
+++ scripts/Summary Information Script.R
@@ -0,0 +1,19 @@
+library(tidyr)
+get_summary_info <- function(dataset) {
+  ret <- list()
+  ret$column_name <- colnames(dataset)
+  ret$most_bedrooms <- dataset %>% 
+    filter(bedrooms == max(bedrooms)) %>% 
+    pull(bedrooms)
+  ret$most_expensive_room <- dataset %>% 
+    filter(price == max(price)) %>% 
+    pull(price)
+  ret$cheapest_room <- dataset %>% 
+    filter(price == min(price)) %>% 
+    select(price)
+  ret$largest_living_room_ft <- dataset %>%
+    filter(sqft_living == max(sqft_living)) %>% 
+    select(sqft_living)
+  return (ret)
+} 
+get_summary_info(house_sales)
---
+++ scripts/barplot scripts.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(ggplot2)
+house_sales <- read.csv("data/house_sales.csv", stringsAsFactors = FALSE)
+get_plot_info <- function(dataset) {
+  twob_twob <- dataset %>% 
+    filter(bedrooms == 2, bathrooms == 2) %>% 
+    group_by(floors) %>% 
+    summarise(price = mean(price))
+  
+  return(ggplot(twob_twob) + geom_col(mapping = aes(x = floors, y = price)) +
+    labs(
+      title = "Sales of 2B2B house from different floors",
+      x = "Floors",
+      y = "Price"
+    ))
+}
+
+get_plot_info(house_sales)
+
