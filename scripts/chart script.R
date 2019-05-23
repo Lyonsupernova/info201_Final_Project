e2608c4b8c47d2ccf3dfcd0ca9d2e8c1d8e07a4b
Amara <amaraperry9@gmail.com>
2019-05-22 23:35:45 -0700

    Create data table

---
+++ scripts/chart script.R
@@ -0,0 +1,22 @@
+house_sales <- read.csv("data/house_sales.csv", stringsAsFactors = FALSE)
+
+library(dplyr)
+
+# finds average square footage, bedroom, bath, and price for a given range of square footage
+# provides insight on expected cost of housing and number of beds/bath per range of square footage
+get_chart <- function(dataset) {
+  dataset <- select(dataset, c(price, bedrooms, bathrooms, sqft_living))
+  dataset$sqft_range <- cut(dataset$sqft_living, breaks = seq(0, 14000, by=1000), right = TRUE, dig.lab=10, 
+                       labels = c("0 - 1,000", "1,000 - 2,000", "2,000 - 3,000" , "3,000 - 4,000", "4,000-5,000",
+                                  "5,000 - 6,000", "6,000 - 7,000", "7,000 - 8,000", "8,000 - 9,000", "9,000 - 10,000",
+                                  "10,000 - 11,000", "11,000 - 12,000", "12,000 - 13,000", "13,000 - 14,000"))
+  dataset %>%
+    group_by(sqft_range) %>%
+    summarize(avg_sqft = format(round(mean(sqft_living), 2), big.mark=","), 
+              mean_bd = format(round(mean(bedrooms), 1)),
+              mean_bath = round(mean(bathrooms), 1), 
+              mean_price = format(mean(price), big.mark=","))
+}
+
+
+View(get_chart(dt))
