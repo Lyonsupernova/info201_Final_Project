6607414865c509be3c436fd560362ce1c5461dfd
George Ma <george9977@icloud.com>
2019-05-23 22:09:37 -0700

    finish lineplot

---
+++ linelot script.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+house_sales <- read.csv("data/house_sales.csv", stringsAsFactors = FALSE)
+
+year_price_plot <- function(dataset) {
+  year_price <- dataset %>%
+    select(yr_built, price) %>%
+    group_by(yr_built) %>%
+    summarise(ave_price = mean(price))
+
+  ggplot(year_price, aes(x = yr_built, y = ave_price)) +
+    geom_line() +
+    labs(
+      title = "Relationship between Built Year and Average Price",
+      x = "Built Year",
+      y = "Average Price"
+    ) +
+    scale_x_continuous(breaks = seq(1900, 2015, by = 10)) +
+    scale_y_continuous(labels = scales::comma)
+}
+
+year_price_plot(house_sales)
